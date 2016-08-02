e5d2510fd2d0c7dfd85d497d97755b5a026134a5
Enes Anbar <enesanbar@gmail.com>
2016-08-02 14:00:46 +0300

    Create a jinja2 template for the index page

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from jinja2 import Environment, FileSystemLoader
+
 app = Flask(__name__)
+default_name = "Enes Anbar"
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def get_indenticon():
+    name = default_name
+
+    template_env = Environment(loader = FileSystemLoader(searchpath = "."))
+    template = template_env.get_template("index.html")
+
+    return template.render({'name': name})
 
 # executes when the file is called as a standalone program and
 # not when running as part of a larger application.
