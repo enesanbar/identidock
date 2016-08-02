4908a9b6490d94a2a6fcca1f162934eca24356fa
Enes Anbar <enesanbar@gmail.com>
2016-08-02 16:11:23 +0300

    Include tests runner in the cmd file

--- cmd.sh
+++ cmd.sh
@@ -4,7 +4,10 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
     echo "Running development server..."
     exec python "identidock.py"
-else
+elif [ "$ENV" = "UNIT" ]; then
+    echo "Running tests..."
+    exec python tests.py
+else    
     echo "Running production server..."
     exec uwsgi --ini /uwsgi.ini
 fi
