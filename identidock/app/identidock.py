7e287e292768c0205131d19e764260401ebaedae
Enes Anbar <enesanbar@gmail.com>
2016-08-02 14:22:24 +0300

    Add dnmonster service to generate the icon for the default name

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,18 +1,27 @@
-from flask import Flask
+from flask import Flask, Response
 from jinja2 import Environment, FileSystemLoader
+import requests
 
 app = Flask(__name__)
 default_name = "Enes Anbar"
 
 
 @app.route('/')
-def get_indenticon():
+def main_page():
     name = default_name
 
     template_env = Environment(loader = FileSystemLoader(searchpath = "."))
     template = template_env.get_template("index.html")
 
     return template.render({'name': name})
+
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+    r = requests.get('http://dnmonster:8080/monster/{}?size=80'.format(name))
+    image = r.content
+
+    return Response(image, mimetype = 'image/png')
 
 # executes when the file is called as a standalone program and
 # not when running as part of a larger application.
